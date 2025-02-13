4a95399385fc58d3e701543eb2a71442d0a25cfb
Denis Kisina <denis.kisina@cybersecurityworks.com>
2025-02-13 06:25:45 -0600

    initial commit

---
+++ src/main/java/com/example/pomchecker/pom_compliance_checker/PomComplianceCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pomchecker.pom_compliance_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomComplianceCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomComplianceCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pomchecker/pom_compliance_checker/PomComplianceCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pomchecker.pom_compliance_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomComplianceCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
